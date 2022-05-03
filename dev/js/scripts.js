d5ec974ddd9b903d503a2543eae70dd494ca80e8
EricSweeney <ejs363@drexel.edu>
2022-05-03 19:51:54 -0400

    Plugins Added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,6 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin"
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
 
